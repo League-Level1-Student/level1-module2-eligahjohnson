822ee760032e78450cb28e8e3448743cbfa2468c
eligahjohnson <teststudent123@jointheleague.org>
2019-01-08 19:18:30 -0800

    Add files via upload

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+package extra;
+
+public class Person {
+	String Blackpanther ;
+	String Agility ;
+	
+	String getBlackpanther() {
+		return Blackpanther;
+		
+	}
+	String getAgility() {
+		return Agility;
+		
+	}
+ void setBlackpanther(String Blackpanther) {
+	 this.Blackpanther=Blackpanther;
+ }
+
+void setAgility(String Agility) {
+	 this.Agility=Agility;
+}
+}
---
+++ src/Taco.java
@@ -0,0 +1,19 @@
+package extra;
+
+public class Taco {
+
+	private String meat;
+	private String sauce ;
+	String getMeat() {
+		return meat;
+	}
+	
+String getSauce() {
+	return sauce;
+	
+}
+void setMeat(String meat) {
+	this.meat=meat;
+}
+
+}
