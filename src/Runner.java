b70ab539bb34e23376d883e79a47405916cf77ee
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-15 19:19:28 -0800

    Runners

---
+++ src/Runner.java
@@ -0,0 +1,25 @@
+
+
+public class Runner {
+	public static void main(String[] args) {
+		Person Dave = new Person ();
+		Person maxson = new Person ();
+		Person hali = new Person ();
+		Dave.setname("Butthead"); 
+		maxson.setname("crackhead");
+		hali.setname("tweak");
+		Dave.setSuperpower("Farting out of his head");
+		maxson.setSuperpower("Og Tweaking ");
+		hali.setSuperpower(" Tweaking on command");
+//		System.out.println(Dave.getname());
+//		System.out.println(Dave.getSuperpower());
+//		System.out.println(maxson.getname());
+//		System.out.println(maxson.getSuperpower());
+//		System.out.println(hali.getname());
+//		System.out.println(hali.getSuperpower());
+		System.out.println(maxson);
+		System.out.println(hali);
+		System.out.println(Dave);
+	}
+
+}
---
+++ src/runnersea.java
@@ -0,0 +1,9 @@
+
+public class runnersea {
+	public static void main(String[] args) {
+		SeaCreature spongebob = new SeaCreature ("Spongebob");
+		Spongebob.
+		
+	}
+
+}
