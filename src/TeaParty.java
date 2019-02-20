a1b6192d312199830feb41d8440fbda167145688
ilabstaff <ilabstaff@172.16.116.67>
2019-02-19 19:11:53 -0800

    TeaParty

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,12 +1,17 @@
 
 public class TeaParty {
 
-
-	       public String welcome(String name, boolean isWoman, boolean isKnighted)
-	       {
-	    	   
-	       }
-
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		String greeting = "Hello ";
+		if (isWoman == true && isKnighted == true){
+			greeting = greeting + "Lady " + name;
+			
+		}
+		else if  (isWoman == false && isKnighted ) { 
+			greeting = greeting + " Sir" + name ;
+		}
+		
+		return greeting ;
 	}
 
-
+}
